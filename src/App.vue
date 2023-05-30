54fe74750ebdcca73fb655460b8b062212e266ad
Camilla Gustafsson <camilla.gustafsson@jellyhive.se>
2023-05-30 17:06:07 +0200

    Added all necessary elements

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,16 @@
 </script>
 
 <template>
-  <header>header</header>
+  <header>DM-quiz</header>
   <main>
-    <p>main</p>
+    <p class="score-section">du har x poäng av x poäng</p>
+    <p class="question-text">Frågan</p>
+    <div class="answer-section">
+      <button>opt1</button>
+      <button>opt2</button>
+      <button>opt2</button>
+      <button>opt3</button>
+    </div>
   </main>
 </template>
 
